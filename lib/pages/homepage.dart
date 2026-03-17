05bec4bcf021fb3fe9f5fd6f5531359fa67c3f7f
Umair Akbar <umairakbar5665@gmail.com>
2026-03-17 17:48:44 +0500

    Resolved issue regarding UI when Image is uploaded to server

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -38,7 +41,7 @@ class _HomepageState extends State<Homepage> {
   Widget _BuildUI() {
     return DashChat(
       inputOptions: InputOptions(
-        trailing: [IconButton(onPressed: () {}, icon: Icon(Icons.image))],
+        trailing: [IconButton(onPressed: _sendMedia, icon: Icon(Icons.image))],
       ),
       currentUser: currentUser,
       onSend: _sendMessage,
@@ -51,35 +54,54 @@ class _HomepageState extends State<Homepage> {
       Messages = [chatMessage, ...Messages];
     });
     try {
-      List<Content> history = Messages.reversed
-          .map(
-            (e) => Content(
-              parts: [Part.text(e.text)],
-              role: e.user.id == '0' ? 'user' : 'model',
-            ),
-          )
-          .toList();
+      List<Content> history = Messages.reversed.map((e) {
+        if (e.medias != null) {
+          for (var media in e.medias!) {
+            Part.bytes(File(media.url).readAsBytesSync());
+          }
+        }
+        return Content(
+          parts: [Part.text(e.text)],
+          role: e.user.id == '0' ? 'user' : 'model',
+        );
+      }).toList();
       gemini.streamChat(history).listen((value) {
-        String responseChunk = value.output ?? "";
+        String chunk = value.output ?? "";
         setState(() {
-          if (Messages.isNotEmpty && Messages.first.user.id == geminiUser.id) {
+          if (Messages.isNotEmpty && Messages.first.user == geminiUser.id) {
             Messages[0] = ChatMessage(
               user: geminiUser,
               createdAt: DateTime.now(),
-              text: Messages.first.text + responseChunk,
+              text: Messages.first.text + chunk,
             );
           } else {
             ChatMessage response = ChatMessage(
               user: geminiUser,
               createdAt: DateTime.now(),
-              text: responseChunk,
+              text: chunk,
             );
             Messages = [response, ...Messages];
           }
         });
       });
     } catch (e) {
-      print(e);
+      print('Error: $e');
+    }
+  }
+
+  void _sendMedia() async {
+    ImagePicker picker = ImagePicker();
+    XFile? file = await picker.pickImage(source: ImageSource.gallery);
+    if (file != null) {
+      ChatMessage chatMessage = ChatMessage(
+        user: currentUser,
+        createdAt: DateTime.now(),
+        text: 'Describe this image',
+        medias: [
+          ChatMedia(url: file.path, fileName: '', type: MediaType.image),
+        ],
+      );
+      _sendMessage(chatMessage);
     }
   }
 }
